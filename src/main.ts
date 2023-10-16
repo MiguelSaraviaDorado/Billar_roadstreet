34d0e0ecd92c8665d7f9dd60765bf4121052856c
Miguel <migueljesussaraviadorado@gmail.com>
2023-10-15 20:03:17 -0400

    Swagger integrado

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+//IMPORT
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app    = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('Billar RoadStreet')
+  .setDescription('API Para los diferentes registros')
+  .setVersion('1.0')
+  .build()
+  const document = SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(4000);
 }
 bootstrap();
