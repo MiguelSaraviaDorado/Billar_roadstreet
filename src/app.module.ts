2108083e278e9b61d4aa6fe616683856fac3bf79
Miguel <migueljesussaraviadorado@gmail.com>
2023-10-16 19:50:30 -0400

    Conexion a la base de datos exitoso

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
+//IMPORT
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports    : [
+    TypeOrmModule.forRoot({
+      type       : 'postgres',
+      host       : 'localhost',
+      port       : 5432,
+      username   : 'msaravia',
+      password   : '13538451',
+      entities   : [],
+      database   : 'db_roadstreet',
+      synchronize: true,
+    }
+    ),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers  : [AppService],
 })
 export class AppModule {}
